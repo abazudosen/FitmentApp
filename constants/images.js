6c896f643c573546538536f014affba6d283dc80
Abasiono Udosen <abaz@Abasionos-MacBook-Pro.local>
2020-11-01 19:35:44 +0200

    first commit

---
+++ constants/images.js
@@ -0,0 +1,11 @@
+export const sofa = require('../assets/images/sofa.png');
+export const f1 = require('../assets/images/pexels-cottonbro-4503742.jpg');
+export const f2 = require('../assets/images/pexels-cottonbro-4505452.jpg');
+export const f3 = require('../assets/images/pexels-huy-phan-2826787.jpg');
+
+export default {
+  sofa,
+  f1,
+  f2,
+  f3,
+};
---
+++ screens/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import Home from "./Home";
+import ItemDetail from "./ItemDetail";
+
+export {
+    Home,
+    ItemDetail
+};
